e57aa4dc24474f066c4197395d1c43a44e410846
HunterLowBit_Faculdade <HunterLowBit_Faculdade@noreply.com>
2023-10-16 20:16:58 -0300

    CSS FUNCIONOU

--- flask_mysql/app.py
+++ flask_mysql/app.py
@@ -1,18 +1,18 @@
 # flask app with templates and static files
-
-from flask import Flask, render_template, redirect, url_for
-
-app = Flask(__name__)
-
-
-@app.route("/")
+from flask import Flask, render_template
+ 
+# WSGI Application
+# Provide template folder name
+# The default folder name should be "templates" else need to mention custom folder name
+app = Flask(__name__, template_folder='templateFiles', static_folder='staticFiles')
+ 
+# @app.route('/')
+# def welcome():
+#     return "This is the home page of Flask Application"
+ 
+@app.route('/')
 def index():
-    return render_template("index.html")
-
-
-@app.route("/process", methods=["POST"])
-def process():
-    return redirect(url_for("index"))
+    return render_template('index.html')
 
 
 if __name__ == "__main__":
