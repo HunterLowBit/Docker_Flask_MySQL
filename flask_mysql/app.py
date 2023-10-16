61e224932e9e55fc631af0716890952bffb5ea62
HunterLowBit_Faculdade <HunterLowBit_Faculdade@noreply.com>
2023-10-16 20:04:36 -0300

    tentando corrigir problemas

--- flask_mysql/app.py
+++ flask_mysql/app.py
@@ -1,18 +1,18 @@
-# aplicação basica de flask
-from flask import Flask, render_template
-# importando o banco de dados
+# flask app with templates and static files
 
-
-
-
-
+from flask import Flask, render_template, redirect, url_for
 
 app = Flask(__name__)
 
 
 @app.route("/")
-def hello():
+def index():
     return render_template("index.html")
+
+
+@app.route("/process", methods=["POST"])
+def process():
+    return redirect(url_for("index"))
 
 
 if __name__ == "__main__":
